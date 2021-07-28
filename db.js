5bf237458dde9afaea455c506a3696e0da22e0f6
Bagamolbaga <40855088+Bagamolbaga@users.noreply.github.com>
2021-07-29 00:13:47 +0300

    Revert "dfgdf"
    This reverts commit 0cda59dbac2138e42948dbaf4a05b663138fbd35.

--- db.js
+++ db.js
@@ -1,3 +1,10 @@
 const {Sequelize} = require('sequelize')
 
-module.exports = new Sequelize(process.env.DATABASE_URL)
+module.exports = new Sequelize(process.env.DATABASE_URL,
+    {
+        dialect: 'postgres',
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT
+    }
+
+)
