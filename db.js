0cda59dbac2138e42948dbaf4a05b663138fbd35
Bagamolbaga <40855088+Bagamolbaga@users.noreply.github.com>
2021-07-28 23:31:01 +0300

    dfgdf

--- db.js
+++ db.js
@@ -1,10 +1,3 @@
 const {Sequelize} = require('sequelize')
 
-module.exports = new Sequelize(process.env.DATABASE_URL,
-    {
-        dialect: 'postgres',
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT
-    }
-
-)
+module.exports = new Sequelize(process.env.DATABASE_URL)
