531ca2b7b8b95b31dca13d65865cbde49774bd51
Bagamolbaga <40855088+Bagamolbaga@users.noreply.github.com>
2021-07-28 22:55:34 +0300

    last commit before deploy

--- db.js
+++ db.js
@@ -1,9 +1,6 @@
 const {Sequelize} = require('sequelize')
 
-module.exports = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
+module.exports = new Sequelize(process.env.DATABASE_URL,
     {
         dialect: 'postgres',
         host: process.env.DB_HOST,
