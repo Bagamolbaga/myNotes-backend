749b45150e03680345e11f079bd9f47e79fb25cc
Bagamolbaga <40855088+Bagamolbaga@users.noreply.github.com>
2021-07-29 18:19:13 +0300

    add sslmode require

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
-const {Sequelize} = require('sequelize')
+const { Sequelize } = require('sequelize')
 
 const env = process.env.NODE_ENV || 'development'
 
 const devUrl = `postgres://postgres:lehabaga@localhost:5432/my_notes`
 
-const prodUrl = process.env.DATABASE_URL
+const prodUrl = `${process.env.DATABASE_URL}?sslmode=require`
 
 module.exports = new Sequelize(env !== 'development' ? prodUrl : devUrl)
