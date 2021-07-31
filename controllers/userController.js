f2f8d6c276e94da355f4253a2743423f9ae7ffc0
Bagamolbaga <40855088+Bagamolbaga@users.noreply.github.com>
2021-07-31 19:17:43 +0300

    add static and user avatar

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,14 +1,18 @@
 const {User} = require('../models/models')
+const uuid = require('uuid')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
+const path = require('path')
 
-const createJwt = (id, name) => {
-    return jwt.sign({id, name}, process.env.JWT_HASH, {expiresIn: '240h'})
+const createJwt = (id, name, avatar) => {
+    return jwt.sign({id, name, avatar}, process.env.JWT_HASH, {expiresIn: '240h'})
 }
 
 const UserController = {
     registration: async (req, res) => {
         const {name, password} = req.body
+        const {img} = req.files
+
         if (!name || !password) {
             return res.json({message: 'Введите данные'})
         }
@@ -18,9 +22,15 @@ const UserController = {
             return res.json({message: 'Логин занят'})
         }
 
+        let fileName = ''
+        if (img) {
+            fileName = uuid.v4() + '.jpg'
+            img.mv(path.resolve(__dirname, '..', 'static', fileName))
+        }
+
         const mdPassword = await bcrypt.hash(password, 5)
-        const user = await User.create({name, password: mdPassword})
-        const token = createJwt(user.id, user.name)
+        const user = await User.create({name, password: mdPassword, avatar: fileName})
+        const token = createJwt(user.id, user.name, user.avatar)
         return res.json({token})
     },
 
@@ -40,7 +50,7 @@ const UserController = {
             return res.json({message: 'Неверный логин или пароль'})
         }
 
-        const token = createJwt(userBd.id, userBd.name)
+        const token = createJwt(userBd.id, userBd.name, userBd.avatar)
         return res.json({token})
     },
 
@@ -58,7 +68,7 @@ const UserController = {
             const decodeToken = jwt.verify(reqToken, process.env.JWT_HASH)
             req.user = decodeToken
 
-            const token = createJwt(req.user.id, req.user.name)
+            const token = createJwt(req.user.id, req.user.name, req.user.avatar)
 
             res.json({token})
             
--- models/models.js
+++ models/models.js
@@ -4,7 +4,8 @@ const {DataTypes} = require('sequelize')
 const User = sequelize.define('user', {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
     name: {type: DataTypes.STRING, unique: true},
-    password: {type: DataTypes.STRING, allowNull: false}
+    password: {type: DataTypes.STRING, allowNull: false},
+    avatar: {type: DataTypes.STRING}
 })
 
 const Note = sequelize.define('note', {
