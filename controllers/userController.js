39a5a1c5e9cc47de91fd53d7ba273cc708f97f2e
Bagamolbaga <40855088+Bagamolbaga@users.noreply.github.com>
2021-08-11 17:50:28 +0300

    add save user.avatar in firebase storage

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,8 +10,7 @@ const createJwt = (id, name, avatar) => {
 
 const UserController = {
     registration: async (req, res) => {
-        const {name, password} = req.body
-        const {img} = req.files
+        const {name, password, img} = req.body
 
         if (!name || !password) {
             return res.json({message: 'Введите данные'})
@@ -22,14 +21,8 @@ const UserController = {
             return res.json({message: 'Логин занят'})
         }
 
-        let fileName = ''
-        if (img) {
-            fileName = uuid.v4() + '.jpg'
-            img.mv(path.resolve(__dirname, '..', 'static', fileName))
-        }
-
         const mdPassword = await bcrypt.hash(password, 5)
-        const user = await User.create({name, password: mdPassword, avatar: fileName})
+        const user = await User.create({name, password: mdPassword, avatar: img})
         const token = createJwt(user.id, user.name, user.avatar)
         return res.json({token})
     },
